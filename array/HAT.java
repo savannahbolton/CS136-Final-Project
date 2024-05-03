cf415f3e88460f8510425bd9023cdf6792a61a9d
Natalia Avila-Hernandez <na11@williams.edu>
2024-05-03 15:14:58 -0400

    haiii

--- array/HAT.java
+++ array/HAT.java
@@ -2,3 +2,7 @@ package array;
 
 import java.util.*
 import java.io.*
+
+public class HAT<T> implements List<T> {
+	
+}
--- array/Movie.java
+++ array/Movie.java
@@ -2,3 +2,19 @@ package array;
 
 import java.util.*
 import java.io.*
+import array.HAT;
+
+public class Movie{
+	public static void main(String[] args){
+
+
+
+
+
+
+
+
+
+
+	}
+}
