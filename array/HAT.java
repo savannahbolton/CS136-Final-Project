92f8fe224c2d030cc4644703e7a918be52efc42f
Natalia Avila-Hernandez <na11@williams.edu>
2024-05-06 20:57:49 -0400

    hai

--- array/HAT.java
+++ array/HAT.java
@@ -4,5 +4,41 @@ import java.util.*
 import java.io.*
 
 public class HAT<T> implements List<T> {
-	
+
+	//Savannah
+	@Override
+	public void add(T element){
+
+	}
+
+	//Natalia 
+	@Override
+	public void set(int index, T element){
+
+	}
+
+	//Savannah
+	@Override
+	public int size(){
+
+	}
+
+	//Natalia
+	@Override
+	public boolean isEmpty(){
+
+	}
+
+	//Natalia
+	@Override
+	public boolean contains(){
+
+	}
+
+	//Savannah
+	@Override
+	public T get(){
+
+	}
+
 }
