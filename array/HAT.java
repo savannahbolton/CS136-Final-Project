919efd8679bdf5b48f2499891a67e505e0b7dd5b
Savannah Bolton <srb6@williams.edu>
2024-05-07 19:24:46 -0400

    save

--- array/HAT.java
+++ array/HAT.java
@@ -5,15 +5,30 @@ import java.io.*
 
 public class HAT<T> implements List<T> {
 
+	// constants 
 	private static final double GROWTH_FACTOR = 2.0;
+	private static final int DEFAULT_INITIAL_CAPACITY = 4; // a power of 2 default capacity (as given by the rules of a HAT)
 
-	//Instance variables
-	private int size;
+	//instance variables
+	private int size; // number of elements inside the entire array (both parent & child)
+	private int parentPosition; // indices to keep track to keep runtime down (parent array)
+	private int childPosition; // (child array that is attached to each element in the parent)
 	private Object[][] parent;
 
+	// specified capacity constructor 
 	public HAT(int capacity){
-		parent = new Object[capacity][capacity]; 
-		size = 0; 
+		if (capacity % 2 != 0) throw new IllegalArgumentException("capacity is not a power of 2");
+		parent = new Object[capacity][capacity];
+		size = 0;
+		parentPosition = 0;
+		childPosition = 0;
+	}
+
+	// default capacity constructor 
+	pulic HAT(){
+		parent = new Object[DEFAULT_INITIAL_CAPACITY][DEFAULT_INITIAL_CAPACITY];
+		parentPosition = 0; 
+		childPosition = 0; 
 	}
 
 	//Savannah
