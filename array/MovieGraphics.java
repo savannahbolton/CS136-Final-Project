70353e5c8e99c9024f175aa22190943d76bd53e6
Natalia Avila-Hernandez <na11@williams.edu>
2024-05-12 23:35:01 -0400

    started graphics

--- array/MovieGraphics.java
+++ array/MovieGraphics.java
@@ -12,72 +12,108 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.*;
 
-public class MovieGraphics extends JFrame {
+public class MovieGraphics extends JFrame{
+	JFrame mainWindow;
+	JLabel mainLabel;
+	public MovieGraphics(){
+		mainWindow = new JFrame();
+		mainWindow.setSize(800, 800);
 
-	public Movie graphicslist; 
+		//SORT BUTTON
+		JButton sortButton = new JButton("<html><font size='300' color=blue> SORT");
+		sortButton.setBounds(150, 600, 500, 100);
+		sortButton.setBackground(Color.RED);
+		sortButton.setOpaque(true);
 
-	public BufferedImage bf = new BufferedImage(500, 500, 
-			BufferedImage.TYPE_INT_RGB);
+		//MAIN LABEL
+		mainLabel = new JLabel();
+		JPanel label = new JPanel();
+		mainLabel.setText("HATboxd!!!!");
+		label.add(mainLabel);
+		label.setOpaque(true);
+		label.setBackground(Color.WHITE);
+		label.setBounds(300, 200, 200, 200);
+
+		mainWindow.add(sortButton);
+		mainWindow.setLayout(null);
+		mainWindow.setVisible(true);
+		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainWindow.getContentPane().setBackground(Color.frePINK);
+		mainWindow.add(label);
+	}
+	// public Movie graphicslist; 
+
+	// public BufferedImage bf = new BufferedImage(800, 800, 
+	// 		BufferedImage.TYPE_INT_RGB);
 	
-	public MovieGraphics() {
-		this.graphicslist = new Movie(); 
+	// public MovieGraphics() {
+	// 	this.graphicslist = new Movie(); 
 
-		// --- only add code above here in this method --- //
-		// This chunk of code manages the user clicking 
-		CoinMouseListener listener = new CoinMouseListener();
-		addMouseListener(listener);
-		addMouseMotionListener(listener);
+	// 	CoinMouseListener listener = new CoinMouseListener();
+	// 	addMouseListener(listener);
+	// 	addMouseMotionListener(listener);
 		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(500, 500);
-		setVisible(true);
 
-	}
+	// 	JPanel panel = new JPanel();
+	// 	JButton sortButton = new JButton("SORT");
+	// 	panel.add(sortButton);
+	// 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	// 	setSize(800, 800);
+	// 	setVisible(true);
 
-	private int getCoinSquareIndex(int x, int y) {
-		return 0; 
-	}
+	// }
+
+	// private int getCoinSquareIndex(int x, int y) {
+	// 	return 0; 
+	// }
 	
-	public void paint(Graphics g) {
-		Graphics2D g2 = (Graphics2D) bf.getGraphics();;
+	// public void paint(Graphics g) {
+	// 	Graphics2D g2 = (Graphics2D) bf.getGraphics();;
 
-		g2.setPaint(Color.WHITE);
+	// 	g2.setPaint(Color.WHITE);
 
-		g.drawImage(bf,0,0,null);	
-	}
+	// 	g.drawImage(bf,0,0,null);	
+	// }
 		
 	/**
 	 * An inner class to respond to mouse events.
 	 */
-	private class CoinMouseListener implements MouseListener, MouseMotionListener {
+	// private class CoinMouseListener implements MouseListener, MouseMotionListener {
 		
-		public void mousePressed(MouseEvent event) {
+	// 	public void mousePressed(MouseEvent event) {
 			
-		}
+	// 	}
 
-		private boolean clickedOnCoin(int x, int y) {
-			return true; 
-		}
+	// 	private boolean clickedOnCoin(int x, int y) {
+	// 		return true; 
+	// 	}
 		
-		public void mouseReleased(MouseEvent event) {
+	// 	public void mouseReleased(MouseEvent event) {
 			
-		}
+	// 	}
 
-		public void mouseDragged(MouseEvent event) {
+	// 	public void mouseDragged(MouseEvent event) {
 			
-		}
+	// 	}
 
-		public void mouseClicked(MouseEvent event) {}
-		public void mouseEntered(MouseEvent event) {}
-		public void mouseExited(MouseEvent event) {}
-		public void mouseMoved(MouseEvent event) {}
-	}
+	// 	public void mouseClicked(MouseEvent event) {}
+	// 	public void mouseEntered(MouseEvent event) {}
+	// 	public void mouseExited(MouseEvent event) {}
+	// 	public void mouseMoved(MouseEvent event) {}
+	// }
 
 	public static void main(String[] args) {
-		MovieGraphics test = new MovieGraphics(); 
+		new MovieGraphics();
+		// JFrame mainWindow = new JFrame();
+		// mainWindow.setSize(800, 800);
+		// JButton sortButton = new JButton("SORT");
+		// sortButton.setBounds(100, 100, 500, 100);
+		// mainWindow.add(sortButton);
+		// mainWindow.setLayout(null);
+		// mainWindow.setVisible(true);
+
 	}
 
 }
