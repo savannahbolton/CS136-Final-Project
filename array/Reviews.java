61a04e96503de587aece1b6f1e55dbc5fd9ee6e3
Savannah Bolton <srb6@williams.edu>
2024-05-14 10:32:26 -0400

    comments added

--- array/Reviews.java
+++ array/Reviews.java
@@ -9,7 +9,11 @@ import array.Movie.*;
  * A review class specifically for the reviews section of our interface. 
  * -- uses printwriter, filereader & bufferedreader to add text to the 
  * reviews.txt file, and to read that in to an arraylist that stores the 
- * information in strings
+ * information in strings, so that it can be displayed in the graphics
+ * interface 
+ * 
+ * -- the reviews.txt file is empty until a particular user starts adding 
+ * to it with their own reviews 
  */
 public class Reviews{
 	public String title; 
@@ -26,12 +30,14 @@ public class Reviews{
         return scanner.nextLine();
     }
 
+    // to add into the reviews.txt file 
     public void print(String name, String text) throws IOException{
     	PrintWriter pw = new PrintWriter(new FileWriter("array/reviews.txt", true));
 		pw.write(name + ": " + text + "\n"); 
 		pw.close(); 
     }
 
+    // to read in the information from the reviews.txt file so that it can be shown 
     public ArrayList<String> file() throws IOException{
     	FileReader f = new FileReader("array/reviews.txt");
     	BufferedReader t = new BufferedReader(f); 
@@ -55,7 +61,8 @@ public class Reviews{
 
     	return filmReviews = newFilmReviews; 
     }
-	
+
+	// testing to see if it adds to the file and prints the arraylist 
 	public static void main(String[] args) throws IOException{
 		Reviews test = new Reviews(); 
 		System.out.println("HATboxd reviews");
